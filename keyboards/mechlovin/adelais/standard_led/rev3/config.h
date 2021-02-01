8389951115b2ebe9a78cc5c1ee5f487a0993f0c7
alif1994 <alif1994@gmail.com>
2021-02-02 02:43:56 +0800

    Added separate folder for Vial for Adelais

--- keyboards/mechlovin/adelais/standard_led/rev3/config.h
+++ keyboards/mechlovin/adelais/standard_led/rev3/config.h
@@ -2,6 +2,10 @@
 
 #define PRODUCT_ID   0xAD02
 
+#define VIAL_KEYBOARD_UID {0xBC, 0x5A, 0x1B, 0xAE, 0x54, 0xC2, 0xD4, 0x66}
+// #define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+// #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
 #define ENCODERS_PAD_A { A6, A4, B7 }
 #define ENCODERS_PAD_B { A5, A3, B6 }
 #define ENCODER_RESOLUTION 4
